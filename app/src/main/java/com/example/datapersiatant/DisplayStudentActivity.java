751b46c6243fada77a46f05920a0997d760a1a67
durga prasad upadhyaya <durgesh.upadhyaya7@gmail.com>
2019-12-18 10:34:42 +0545

    Final

---
+++ app/src/main/java/com/example/datapersiatant/DisplayStudentActivity.java
@@ -0,0 +1,48 @@
+package com.example.datapersiatant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.datapersiatant.Model.Student;
+import com.example.datapersiatant.Myadapter.Myadapter;
+
+
+import java.util.List;
+
+
+public class DisplayStudentActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private Button btnBackToAddStudents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_student);
+        recyclerView = findViewById(R.id.recyclerViewStudents);
+        btnBackToAddStudents = findViewById(R.id.btnBackToAdd);
+
+        DatabaseHelper dbHelper = new DatabaseHelper(DisplayStudentActivity.this);
+        List<Student> studentList = dbHelper.displayStudents();
+
+        Myadapter studentAdapter = new Myadapter(DisplayStudentActivity.this, studentList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(studentAdapter);
+
+        btnBackToAddStudents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayStudentActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
